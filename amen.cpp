e82b744675bda2de5a352106bde3f3fb413617ee
Simon Hedlund <sermonhedlund@gmail.com>
2014-10-02 20:04:00 +0200

    AMEN LARS

---
+++ amen.cpp
@@ -0,0 +1,19 @@
+
+#import <iostream>
+
+using namespace std;
+
+int main(){
+
+string name;
+    cout << "Hej jag heter Lasse o jag e hörselskadad, vad heter du?\n";
+    cout << "Va?\n";
+
+while(name != "LARS"){
+
+    cin >> name;
+    cout << "Va?\n";
+}
+cout << "Okej?\n";
+return 0;
+}
