80858c79be83ef1d25eccc3035a8782db41036fe
codingInSpace <jonathan.grangien@gmail.com>
2014-10-02 18:01:17 +0200

    hej

--- Helloworld.cpp
+++ Helloworld.cpp
@@ -5,9 +5,9 @@ using namespace std;
 int main(){
 
 string s;
-cout << "Hellew. vad heter due AHDHASASDHASDHHASDH?"
+cout << "Hellew. vad heter due AHDHASASDHASDHHASDH?"; cout << "
 AMEN FUCK U TO! DEN HÄR FILEN FUNKAR INTE EFTERSOM DET HÄR INTE ÄR SKRIVET SOM EN KOMMENTAR.
-cin >> s; //sparar namn japp
+";cin >> s; //sparar namn japp
 
 
 return 0;
