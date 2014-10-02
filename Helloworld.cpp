c173fc0940a77b0c1d6783cfb15dd70baca04279
Alboin <albinbergstrom@gmail.com>
2014-10-02 17:58:18 +0200

    meddelande

--- Helloworld.cpp
+++ Helloworld.cpp
@@ -3,14 +3,11 @@
 using namespace std;
 
 int main(){
-bool Albineterbajs = true;
+bool Albineterbajs = false;
 string s;
-<<<<<<< HEAD
-cout << "Hellew. vad heter dueSDHHASDH?"
-=======
+
 cout << "Hellew. vad heter due AHDHASASDHASDHHASDH?"
 AMEN FUCK U TO! DEN HÄR FILEN FUNKAR INTE EFTERSOM DET HÄR INTE ÄR SKRIVET SOM EN KOMMENTAR.
->>>>>>> 1750e80b81720830c3d2951f5b9fded7736452ed
 cin >> s; //sparar namn japp
 
 while(Albineterbajs){
