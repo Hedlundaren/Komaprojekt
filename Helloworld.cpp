7a9e5abc2c91e976952bff0bddf0f4618efbe18a
Hedlundaren <hedlundaren93@hotmail.com>
2014-10-02 17:59:24 +0200

    hihi

--- Helloworld.cpp
+++ Helloworld.cpp
@@ -5,12 +5,12 @@ using namespace std;
 int main(){
 bool Albineterbajs = true;
 string s;
-<<<<<<< HEAD
-cout << "Hellew. vad heter dueSDHHASDH?"
-=======
-cout << "Hellew. vad heter due AHDHASASDHASDHHASDH?"
-AMEN FUCK U TO! DEN HÄR FILEN FUNKAR INTE EFTERSOM DET HÄR INTE ÄR SKRIVET SOM EN KOMMENTAR.
->>>>>>> 1750e80b81720830c3d2951f5b9fded7736452ed
+
+cout << "Hellew. vad heter dueSDHHASDH?";
+
+cout << "Hellew. vad heter due AHDHASASDHASDHHASDH?";
+//AMEN FUCK U TO! DEN HÄR FILEN FUNKAR INTE EFTERSOM DET HÄR INTE ÄR SKRIVET SOM EN KOMMENTAR.
+
 cin >> s; //sparar namn japp
 
 while(Albineterbajs){
