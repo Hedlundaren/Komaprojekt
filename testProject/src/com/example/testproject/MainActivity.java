cdbbbaae9bcb4de1510d435ccd9c6135a156eb8f
Jonathan <jonathan.grangien@gmail.com>
2014-11-05 21:25:57 +0100

    Try to implement solution -- force closes

--- testProject/src/com/example/testproject/MainActivity.java
+++ testProject/src/com/example/testproject/MainActivity.java
@@ -1,22 +1,70 @@
 package com.example.testproject;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnTouchListener{
 
+	private ImageView mImageView;
+	private ViewGroup mRrootLayout;
+	private int _xDelta;
+	private int _yDelta;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
+        //findViewById(R.id.background1).setOnDragListener(new MyDragListener());
         //img.setOnTouchListener(new View.OnDragListener() {}
+        
+        mRrootLayout = (ViewGroup) findViewById(R.id.root);
+		mImageView = (ImageView) mRrootLayout.findViewById(R.id.background1);
+		
+		RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(150, 150);
+		mImageView.setLayoutParams(layoutParams);
+		mImageView.setOnTouchListener(this);
+        
     }
-
-
+    
+    public boolean onTouch(View view, MotionEvent event) {
+		final int X = (int) event.getRawX();
+		final int Y = (int) event.getRawY();
+		switch (event.getAction() & MotionEvent.ACTION_MASK) {
+		case MotionEvent.ACTION_DOWN:
+			RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
+			_xDelta = X - lParams.leftMargin;
+			_yDelta = Y - lParams.topMargin;
+			break;
+		case MotionEvent.ACTION_UP:
+			break;
+		case MotionEvent.ACTION_POINTER_DOWN:
+			break;
+		case MotionEvent.ACTION_POINTER_UP:
+			break;
+		case MotionEvent.ACTION_MOVE:
+			RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view
+					.getLayoutParams();
+			layoutParams.leftMargin = X - _xDelta;
+			layoutParams.topMargin = Y - _yDelta;
+			layoutParams.rightMargin = -250;
+			layoutParams.bottomMargin = -250;
+			view.setLayoutParams(layoutParams);
+			break;
+		}
+		mRrootLayout.invalidate();
+		return true;
+	}
+    
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
