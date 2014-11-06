ce776dec241d23f87cccdb8ff56efcf04ceb0b50
Jonathan <jonathan.grangien@gmail.com>
2014-11-06 21:34:23 +0100

    copied code

--- testProject/src/com/example/testproject/MainActivity.java
+++ testProject/src/com/example/testproject/MainActivity.java
@@ -1,51 +1,36 @@
 package com.example.testproject;
 
-import android.support.v7.app.ActionBarActivity;
-import android.media.MediaPlayer;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.view.View.OnClickListener;
+import android.app.Activity;
+import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity{
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        Button one = (Button)this.findViewById(R.id.button2);
-        final MediaPlayer mp = MediaPlayer.create(this, R.raw.wilhelmscream);
-        one.setOnClickListener(new OnClickListener(){
-
-        	@Override
-            public void onClick(View v) {
-                mp.start();
+public class MainActivity extends Activity {
+   static final LatLng TutorialsPoint = new LatLng(21 , 57);
+   private GoogleMap googleMap;
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+      try { 
+            if (googleMap == null) {
+               googleMap = ((MapFragment) getFragmentManager().
+               findFragmentById(R.id.map)).getMap();
             }
+         googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+         Marker TP = googleMap.addMarker(new MarkerOptions().
+         position(TutorialsPoint).title("TutorialsPoint"));
 
-        }); 
-    }
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
 
+   }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
